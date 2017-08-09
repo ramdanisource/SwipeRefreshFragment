dd08746fda3432081414ec1e282ba9fb5d4123b4
ramdanix <ramdani.rizki19@gmail.com>
2017-08-09 09:32:48 +0700

    change URL domain

--- app/src/main/java/com/labs/ramdanix/swiperefreshfragment/Config.java
+++ app/src/main/java/com/labs/ramdanix/swiperefreshfragment/Config.java
@@ -6,7 +6,8 @@ package com.labs.ramdanix.swiperefreshfragment;
 
 public class Config {
 
-    public static final String URL_FULL = "http://apisimanset.trycatch.id/api/v1/peminjaman/all/1";
+//    public static final String URL_FULL = "http://apisimanset.trycatch.id/api/v1/peminjaman/all/1";
+    public static final String URL_FULL = "http://apisimanset.trycatch.id/api/v1/peminjaman/history/all/no-token/1";
     public static final String TAG_NAMA = "nama";
     public static final String TAG_KELAS = "kelas";
     public static final String TAG_WAKTU_PINJAM = "waktu_pinjam";
